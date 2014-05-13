a209d5d405608d41f53d26cade3697db522ac3ef
lroth <lukasz.roth@gmail.com>
2014-05-13 23:19:15 +0200

    Routing with annotations and method definition

--- src/Btn/AppBundle/Controller/DefaultController.php
+++ src/Btn/AppBundle/Controller/DefaultController.php
@@ -4,12 +4,18 @@ namespace Btn\AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController
 {
-    public function indexAction(Request $request)
+    /**
+     * @Route("/index", defaults={"location" = "bitnoise office from /index"})
+     * @Route("/{location}", defaults={"location" = "bitnoise office from /"}, name="homepage")
+     * @Method("GET")
+     **/
+    public function indexAction($location)
     {
-        $location = $request->get('location', 'bitnoise office');
 
         return new Response('Hello world from ' . $location);
     }
