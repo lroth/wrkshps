499b27655a8020d79e1bf80f5a39ec024404e90c
lroth <lukasz.roth@gmail.com>
2014-05-15 23:35:39 +0200

    Query categories from db and show them on sidebar (+create Platform entity)

--- src/Btn/AppBundle/Controller/CategoryController.php
+++ src/Btn/AppBundle/Controller/CategoryController.php
@@ -4,7 +4,11 @@ namespace Btn\AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+
+use Btn\AppBundle\Entity\Category;
+use Btn\AppBundle\Entity\Platform;
 
 class CategoryController extends BaseController
 {
@@ -15,7 +19,48 @@ class CategoryController extends BaseController
     public function indexAction()
     {
         // get all categories from db
+        $categories = $this->getDoctrine()
+            ->getRepository('BtnAppBundle:Category')
+            ->findAll();
 
-        return [];
+        return ['categories' => $categories];
+    }
+
+    /**
+     * @Route("/fill_in_categories")
+     **/
+    public function fillAction()
+    {
+        $categories = ['Action', 'RPG', 'Sport'];
+        $em = $this->getDoctrine()->getManager();
+
+        foreach ($categories as $name) {
+            $category = new Category();
+            $category->setName($name);
+            $em->persist($category);
+        }
+
+        $em->flush();
+
+        return new Response('categories created');
+    }
+
+    /**
+     * @Route("/fill_in_platforms")
+     **/
+    public function fillPlatformsAction()
+    {
+        $platforms = ['PS3', 'PS4', 'PS VITA'];
+        $em = $this->getDoctrine()->getManager();
+
+        foreach ($platforms as $name) {
+            $platform = new Platform();
+            $platform->setName($name);
+            $em->persist($platform);
+        }
+
+        $em->flush();
+
+        return new Response('platforms created');
     }
 }
---
+++ src/Btn/AppBundle/Entity/Platform.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Btn\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="platform")
+ */
+class Platform
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Platform
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
